9c81d15aa33cd05239c756d75119d79303898f86
Andreacss <b021910194@student.utem.edu.my>
2022-04-19 22:55:56 +0800

    index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app= express ();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser, json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World, from express');
+});
+
+//express()
+  //.use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('pages/index'))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
